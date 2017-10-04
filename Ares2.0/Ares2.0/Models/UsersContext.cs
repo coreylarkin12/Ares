4e4fa46bf1004bee583a12f3590f1a19d22dd142
Corey Larkin <clarkin17@gmail.com>
2017-10-03 20:39:29 -0400

    put in entity framework model

--- Ares2.0/Ares2.0/Models/UsersContext.cs
+++ Ares2.0/Ares2.0/Models/UsersContext.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ares2.Models
 {
-    public class UsersContext
+    public class UsersContext : DbContext
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- Ares2.0/Ares2.0/wwwroot/Scripts/exercises/websocket-exercises.js
+++ Ares2.0/Ares2.0/wwwroot/Scripts/exercises/websocket-exercises.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="../jquery-3.1.1.js" />
-/// <reference path="../jquery.signalR-2.2.1.js" />
+﻿/// <reference path="../jquery.signalR-2.2.1.js" />
+/// <reference path="../jquery-3.1.1.js" />
 
 $(document).ready(function () {
     
