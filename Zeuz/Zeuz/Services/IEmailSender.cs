3ab07333d16b9f675ef569a151af4a1abcc926b2
Corey Larkin <clarkin17@gmail.com>
2017-10-06 12:44:40 -0400

    New Project Zeus Initial Push

---
+++ Zeuz/Zeuz/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zeuz.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
