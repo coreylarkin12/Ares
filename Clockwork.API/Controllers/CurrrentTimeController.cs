d8b62ad265f56b5893abe336247d49f37514ae75
Corey Larkin <clarkin@ohiohealthgroup.com>
2018-09-05 20:53:25 -0400

    Add project files.

---
+++ Clockwork.API/Controllers/CurrrentTimeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Clockwork.API.Controllers
+{
+    //[Route("api/[controller]")]
+    public class CurrentTimeController : Controller
+    {
+        // GET api/currenttime
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult Get()
+        {
+            var utcTime = DateTime.UtcNow;
+            var serverTime = DateTime.Now;
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            var returnVal = new CurrentTimeQuery
+            {
+                UTCTime = utcTime,
+                ClientIp = ip,
+                Time = serverTime
+            };
+
+          
+            using (var db = new ClockworkContext())
+            {
+                db.CurrentTimeQueries.Add(returnVal);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
+                {
+                    Console.WriteLine(" - {0}", CurrentTimeQuery.UTCTime);
+                }
+            }
+
+            return Json(returnVal, new Newtonsoft.Json.JsonSerializerSettings() { DateFormatString = "hh:mm tt" });
+        }
+
+        [HttpGet]
+        [Route("api/[action]")]
+        public IActionResult GetAll()
+        {
+            var context = new ClockworkContext();
+
+            var selectAll = context.CurrentTimeQueries.OrderByDescending(x => x.CurrentTimeQueryId).ToList();
+
+            ReadOnlyCollection<TimeZoneInfo> tz;
+            tz = TimeZoneInfo.GetSystemTimeZones();
+
+            return Json(selectAll, new Newtonsoft.Json.JsonSerializerSettings() { DateFormatString = "hh:mm tt" });
+
+        }
+
+        [HttpGet]
+        [Route("api/[action]")]
+        public IActionResult SelectTimeZone()
+        {
+
+            ReadOnlyCollection<TimeZoneInfo> tz;
+            tz = TimeZoneInfo.GetSystemTimeZones();
+
+            tz.OrderBy(x => x.DisplayName).ToList();
+
+            return Json(tz, new Newtonsoft.Json.JsonSerializerSettings() { DateFormatString = "hh:mm tt" });
+
+        }
+    }
+}
---
+++ Clockwork.Core/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace Clockwork.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllEntries()
+        {
+            return View();
+        }
+
+        public IActionResult SelectTimeZone()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Clockwork"] = "Clockwork Project";
+            ViewData["Author"] = "Developer: Corey Larkin.";
+            ViewData["Message"] = "Application that makes a call to an API and returns the current time.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
