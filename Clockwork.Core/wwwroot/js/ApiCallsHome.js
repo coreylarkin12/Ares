3e8614161c2f74e1ada75ab6d40859f1215f1b41
Corey Larkin <clarkin@ohiohealthgroup.com>
2018-09-06 09:54:56 -0400

    Moved javaScript out of the views into their own files. Cleaning up some code structure

---
+++ Clockwork.Core/wwwroot/js/ApiCallsHome.js
@@ -0,0 +1,26 @@
+﻿
+function UserAction() {
+    $.get("http://localhost:58148/api/currenttime").done(function (data) {
+        $('#clientIp').html(data.ClientIp);
+        $('#currentTimeQueryId').html(data.CurrentTimeQueryId);
+        $('#time').html(data.Time);
+        $('#utcTime').html(data.UTCTime);
+
+        var row = '<tr> <td>' + data.ClientIp + '</td> <td> ' + data.CurrentTimeQueryId + ' </td><td>' + data.Time + '</td><td>' + data.UTCTime + '</td></tr>'
+        $(row).insertAfter("#getAllTable tr:first")
+    });
+}
+
+function formatItem(item) {
+    return '<td>' + item.ClientIp + '</td> <td> ' + item.CurrentTimeQueryId + ' </td><td>' + item.Time + '</td><td>' + item.UTCTime + '</td>';
+}
+
+function GetAll() {
+    $.get("http://localhost:58148/api/getall").done(function (data) {
+        $.each(data, function (key, item) {
+            $('<tr>', { html: formatItem(item) }).appendTo($("#getAllTable"));
+            $('#show').attr("disabled", true)
+        })
+    });
+
+}
---
+++ Clockwork.Core/wwwroot/js/SelectTimeZone.js
@@ -0,0 +1,19 @@
+﻿function UserAction() {
+    $.get("http://localhost:58148/api/selecttimezone").done(function (data) {
+        $.each(data, function (i, item) {
+            $('#mySelect').append($('<option>', {
+                value: item.BaseUtcOffset,
+                text: item.DisplayName
+            }));
+        });
+    });
+}
+
+function ConvertTime() {
+    var selectedTimeZone = $('#mySelect').find(":selected").val();
+    var name = $('#mySelect').find(":selected").text();
+
+    selectedTimeZone.toString();
+
+    $('#converted').append('<li>  ' + selectedTimeZone + '  ---  ' + name + '</li>')
+}
