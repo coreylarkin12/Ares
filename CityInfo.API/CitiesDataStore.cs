ed14e083775e79b51998a5914ce9d1a656361d99
Corey Larkin <clarkin@ohiohealthgroup.com>
2018-09-12 10:32:48 -0400

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Most visited park."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "The Shoe",
+                            Description = "Buckeyes football"
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Columbus",
+                    Description = "The best city ever."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with the big tower."
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/CityInfoContextExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API
+{
+    public static class CityInfoContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+            if (context.Cities.Any())
+            {
+                return;
+            }
+
+            // init seed data
+            var cities = new List<City>()
+            {
+                new City()
+                {
+                    Name = "New York City",
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the united states"
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        },
+                    }
+                },
+                new City()
+                {
+                    Name = "Columbus",
+                    Description = "The best city in Ohio.",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "The HorseShoe",
+                            Description = "Where the Ohio State Buckeyes Play football on Saturday's."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "German Village",
+                            Description = "Old Historical village near downtown Columbus."
+                        },
+                    }
+                },
+                new City()
+                {
+                    Name = "Paris",
+                    Description = "The one with that big tower.",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "The Louvre",
+                            Description = "The worlds largest museum."
+                        },
+                    }
+                }
+            };
+
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+    }
+}
